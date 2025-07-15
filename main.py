50e5ecdceeb0ab6f85fc9104d8ce15e05ef3e851
ShadowBloodBot <joseph.azar001@gmail.com>
2025-07-15 16:39:24 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-from mirage.keep_alive import keep_alive
-from mirage.config import setup_bot
+from keep_alive import keep_alive
+from config import setup_bot
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -12,6 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 intents.guilds = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
