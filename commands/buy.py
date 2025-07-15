506889f260f76b4c8b4d8be2a46581a054b484a1
ShadowBloodBot <joseph.azar001@gmail.com>
2025-07-15 14:41:48 +1000

    Delete commands directory

--- commands/buy.py
+++
@@ -1,4 +0,0 @@
-from discord.ext import commands
-class Buy(commands.Cog):
-    def __init__(self, bot): self.bot = bot
-async def setup(bot): await bot.add_cog(Buy(bot))
--- commands/crafting.py
+++
@@ -1,4 +0,0 @@
-from discord.ext import commands
-class Crafting(commands.Cog):
-    def __init__(self, bot): self.bot = bot
-async def setup(bot): await bot.add_cog(Crafting(bot))
